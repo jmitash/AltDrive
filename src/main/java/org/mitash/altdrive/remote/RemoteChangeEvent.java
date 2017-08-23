aadb26b5b3ef491c5344e95935c5f663000c2183
Jacob Mitash <jacobmitash@gmail.com>
2017-08-22 21:58:22 -0500

    Added new RemoteChangeEvent and published it in RemoteWatcherThread

---
+++ src/main/java/org/mitash/altdrive/remote/RemoteChangeEvent.java
@@ -0,0 +1,24 @@
+package org.mitash.altdrive.remote;
+
+import org.mitash.altdrive.event.Event;
+
+/**
+ * This event is fired when a change in the remote drive is detected.
+ * @author jacob
+ */
+public class RemoteChangeEvent extends Event {
+
+    private final String fileId;
+
+    RemoteChangeEvent(String fileId) {
+        this.fileId = fileId;
+    }
+
+    /**
+     * Gets the file ID of the file that was changed.
+     * @return the file ID
+     */
+    public String getFileId() {
+        return fileId;
+    }
+}
--- src/main/java/org/mitash/altdrive/remote/RemoteWatcherThread.java
+++ src/main/java/org/mitash/altdrive/remote/RemoteWatcherThread.java
@@ -3,6 +3,7 @@ package org.mitash.altdrive.remote;
 import com.google.api.services.drive.model.Change;
 import com.google.api.services.drive.model.ChangeList;
 import org.mitash.altdrive.drive.AltDrive;
+import org.mitash.altdrive.event.EventPublisher;
 import org.mitash.altdrive.logger.ADLoggerInjector;
 
 import java.util.logging.Logger;
@@ -16,16 +17,18 @@ public class RemoteWatcherThread extends Thread {
     private final static Logger LOGGER = ADLoggerInjector.buildLogger(RemoteWatcherThread.class.getName());
 
     private final AltDrive altDrive;
+    private final EventPublisher eventPublisher;
 
     /**
      * Initializes the thread as a daemon and sets the thread name.
      * @param altDrive the Drive to watch
      */
-    RemoteWatcherThread(AltDrive altDrive) {
+    RemoteWatcherThread(AltDrive altDrive, EventPublisher eventPublisher) {
         super("RemoteWatcher");
         this.setDaemon(true);
 
         this.altDrive = altDrive;
+        this.eventPublisher = eventPublisher;
     }
 
     /**
@@ -48,7 +51,8 @@ public class RemoteWatcherThread extends Thread {
                 ChangeList changes = altDrive.getChanges(pageToken);
                 for (Change change : changes.getChanges()) {
                     LOGGER.finer("Received change: " + change.toString());
-                    //TODO: publish changes
+                    RemoteChangeEvent event = new RemoteChangeEvent(change.getFileId());
+                    eventPublisher.publishEvent(event);
                 }
                 if (changes.getNewStartPageToken() != null) {
                     // Last page, save token for next poll
