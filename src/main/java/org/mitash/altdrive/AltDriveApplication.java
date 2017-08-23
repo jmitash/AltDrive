62cc8b8662cef09b802abd3cd9b72b643ce20f5e
Jacob Mitash <jacobmitash@gmail.com>
2017-08-23 16:37:20 -0500

    Added main loop logic for handling events

--- src/main/java/org/mitash/altdrive/AltDriveApplication.java
+++ src/main/java/org/mitash/altdrive/AltDriveApplication.java
@@ -1,12 +1,56 @@
 package org.mitash.altdrive;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.mitash.altdrive.drive.AltDrive;
+import org.mitash.altdrive.drive.DriveModule;
+import org.mitash.altdrive.event.Event;
+import org.mitash.altdrive.event.EventModule;
+import org.mitash.altdrive.event.EventPublisher;
+import org.mitash.altdrive.logger.ADLoggerInjector;
+import org.mitash.altdrive.remote.RemoteWatcher;
+import org.mitash.altdrive.remote.RemoteWatcherModule;
+
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AltDriveApplication {
 
     public static final String APPLICATION_NAME = "AltDrive";
 
-    public static void main(String[] args) throws IOException {
+    private final static Logger LOGGER = ADLoggerInjector.buildLogger(AltDriveApplication.class.getName());
 
+    private void start() {
+        Injector injector = Guice.createInjector(
+                new DriveModule(),
+                new EventModule(),
+                new RemoteWatcherModule());
+
+        try {
+            injector.getInstance(AltDrive.class).initialize();
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Could not initialize Drive", e);
+            return;
+        }
+
+        injector.getInstance(RemoteWatcher.class).startWatching();
+        EventPublisher eventPublisher = injector.getInstance(EventPublisher.class);
+
+        while (!Thread.interrupted()) {
+            Event event;
+            try {
+                event = eventPublisher.dequeueEvent();
+            } catch (InterruptedException e) {
+                LOGGER.log(Level.WARNING, "Interrupted while waiting for next event", e);
+                return;
+            }
+
+            eventPublisher.publishEvent(event);
+        }
+    }
+
+    public static void main(String[] args) {
+        new AltDriveApplication().start();
     }
 }
