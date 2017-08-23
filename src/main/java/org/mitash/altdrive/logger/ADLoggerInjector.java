e96f95716d9de74b124bf0ec3b50e31f1d744a60
Jacob Mitash <jacobmitash@gmail.com>
2017-08-23 17:03:55 -0500

    Added properties for logging levels

--- src/main/java/org/mitash/altdrive/logger/ADLoggerInjector.java
+++ src/main/java/org/mitash/altdrive/logger/ADLoggerInjector.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -15,6 +16,15 @@ import java.util.logging.Logger;
  * @author jacob
  */
 public class ADLoggerInjector<T> implements MembersInjector<T> {
+
+    private final static Logger LOGGER = buildLogger(ADLoggerInjector.class.getName());
+    private final static Level LEVEL_LOGGER_DEFAULT = Level.INFO;
+    private final static Level LEVEL_CONSOLE_DEFAULT = Level.INFO;
+    private final static Level LEVEL_FILE_DEFAULT = Level.INFO;
+    private static boolean loggedLoggerLevelError = false;
+    private static boolean loggedConsoleLevelError = false;
+    private static boolean loggedFileLevelError = false;
+
     private final Field field;
     private final Logger logger;
 
@@ -30,7 +40,7 @@ public class ADLoggerInjector<T> implements MembersInjector<T> {
         field.setAccessible(true);
 
         Logger logger;
-        if((logger = cachedLoggers.get(field.getDeclaringClass().getName())) != null) {
+        if ((logger = cachedLoggers.get(field.getDeclaringClass().getName())) != null) {
             this.logger = logger;
         } else {
             this.logger = buildLogger(field.getDeclaringClass().getName());
@@ -63,7 +73,66 @@ public class ADLoggerInjector<T> implements MembersInjector<T> {
         };
         handler.setFormatter(prettyFormatter);
         logger.addHandler(handler);
-
+        logger.setLevel(getLoggerLevel());
+        handler.setLevel(getConsoleLevel());
         return logger;
+    }
+
+    private static Level getLoggerLevel() {
+        if (loggedLoggerLevelError) {
+            //Prevents spamming multiple of the same error
+            return LEVEL_LOGGER_DEFAULT;
+        }
+
+        String levelProperty = System.getProperty("logger.logger.level", "INFO");
+        Level level;
+        try {
+            level = Level.parse(levelProperty);
+        } catch (IllegalArgumentException e) {
+            LOGGER.log(Level.WARNING, "Invalid logger logging level: " + levelProperty);
+            LOGGER.warning("Defaulting to " + LEVEL_LOGGER_DEFAULT.getName());
+            loggedLoggerLevelError = true;
+            return LEVEL_LOGGER_DEFAULT;
+        }
+
+        return level;
+    }
+
+    private static Level getConsoleLevel() {
+        if (loggedConsoleLevelError) {
+            //Prevents spamming multiple of the same error
+            return LEVEL_CONSOLE_DEFAULT;
+        }
+
+        String levelProperty = System.getProperty("logger.console.level", "INFO");
+        Level level;
+        try {
+            level = Level.parse(levelProperty);
+        } catch (IllegalArgumentException e) {
+            LOGGER.log(Level.WARNING, "Invalid console logging level: " + levelProperty);
+            LOGGER.warning("Defaulting to " + LEVEL_CONSOLE_DEFAULT.getName());
+            loggedConsoleLevelError = true;
+            return LEVEL_CONSOLE_DEFAULT;
+        }
+        return level;
+    }
+
+    private static Level getFileLevel() {
+        if (loggedFileLevelError) {
+            //Prevents spamming multiple of the same error
+            return LEVEL_FILE_DEFAULT;
+        }
+
+        String levelProperty = System.getProperty("logger.file.level", "INFO");
+        Level level;
+        try {
+            level = Level.parse(levelProperty);
+        } catch (IllegalArgumentException e) {
+            LOGGER.log(Level.WARNING, "Invalid file logging level: " + levelProperty);
+            LOGGER.warning("Defaulting to " + LEVEL_FILE_DEFAULT.getName());
+            loggedFileLevelError = true;
+            return LEVEL_FILE_DEFAULT;
+        }
+        return level;
     }
 }
